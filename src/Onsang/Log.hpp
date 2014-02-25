a740cf339920510ba0e7f756b812f914b1b6f65d
Coranna Howard <me@komiga.com>
2014-02-24 23:52:18 -0500

    Log: added report_error() functions for Hord and Onsang.

--- src/Onsang/Log.hpp
+++ src/Onsang/Log.hpp
@@ -11,12 +11,36 @@ see @ref index or the accompanying LICENSE file for full text.
 #define ONSANG_LOG_HPP_
 
 #include <Onsang/config.hpp>
+#include <Onsang/Error.hpp>
 
 #include <Hord/Log.hpp>
+#include <Hord/Error.hpp>
 
 namespace Onsang {
 
 namespace Log = Hord::Log;
+
+inline void
+report_error(
+	Hord::Error const& err
+) {
+	Log::acquire(Log::error)
+		<< '[' << Hord::get_error_name(err.get_code()) << ']'
+		<< '\n' << err.get_message()
+		<< '\n'
+	;
+}
+
+inline void
+report_error(
+	Onsang::Error const& err
+) {
+	Log::acquire(Log::error)
+		<< '[' << get_error_name(err.get_code()) << ']'
+		<< '\n' << err.get_message()
+		<< '\n'
+	;
+}
 
 } // namespace Onsang
 
