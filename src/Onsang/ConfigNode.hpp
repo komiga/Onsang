1001bb2d71f5fa9ed0fdce2a3e3d902eda8fb5fb
Coranna Howard <me@komiga.com>
2014-02-27 14:28:23 -0500

    Corrected copyright holder name.¹
    1: It's your legal name, doofus.

--- src/Onsang/ConfigNode.hpp
+++ src/Onsang/ConfigNode.hpp
@@ -3,8 +3,8 @@
 @file ConfigNode.hpp
 @brief Configuration node.
 
-@author Tim Howard
-@copyright 2013-2014 Tim Howard under the MIT license;
+@author Timothy Howard
+@copyright 2013-2014 Timothy Howard under the MIT license;
 see @ref index or the accompanying LICENSE file for full text.
 */
 
