fa7ed8481e2e165409a6d189f9c54bd1a3f5b5b7
Coranna Howard <me@komiga.com>
2014-09-21 17:34:20 -0700

    UI: new module; added Beard namespace imports.

---
+++ src/Onsang/UI/Defs.hpp
@@ -0,0 +1,36 @@
+/**
+@file UI/Defs.hpp
+@brief UI definitions.
+
+@author Timothy Howard
+@copyright 2013-2014 Timothy Howard under the MIT license;
+see @ref index or the accompanying LICENSE file for full text.
+*/
+
+#pragma once
+
+#include <Onsang/config.hpp>
+#include <Onsang/utility.hpp>
+
+#include <Beard/geometry.hpp>
+#include <Beard/ui/Defs.hpp>
+#include <Beard/ui/Widget/Defs.hpp>
+
+namespace Onsang {
+namespace UI {
+
+// Forward declarations
+enum class OnsangWidgetType : unsigned;
+
+using namespace Beard;
+using namespace Beard::ui;
+
+enum class OnsangWidgetType : unsigned {
+	BASE = enum_cast(UI::Widget::Type::USERSPACE_BASE) + 0xFF00,
+	TabbedContainer,
+	Grid,
+	ObjectView,
+};
+
+} // namespace UI
+} // namespace Onsang
