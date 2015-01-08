7e8138a5d3762f0590ad0106093d89ae1814b507
Coranna Howard <me@komiga.com>
2015-01-07 21:00:53 -0500

    Updated copyrights and trimmed down file headers.

--- src/Onsang/detail/Hord/gr_unconfigure.hpp
+++ src/Onsang/detail/Hord/gr_unconfigure.hpp
@@ -1,10 +1,8 @@
 /**
+@copyright MIT license; see @ref index or the accompanying LICENSE file.
+
 @file
 @brief Unconfigure GR (Hord).
-
-@author Timothy Howard
-@copyright 2013-2014 Timothy Howard under the MIT license;
-see @ref index or the accompanying LICENSE file for full text.
 */
 
 #include <Hord/detail/gr_undefine.hpp>
