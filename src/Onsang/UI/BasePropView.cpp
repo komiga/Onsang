8a93f326003680b2a6c7d101e47d167bc40d8a39
Coranna Howard <me@komiga.com>
2015-01-22 23:17:23 -0500

    UI/BasePropView: handle metadata signal_cell_edited; update metadata grid as notified by metadata commands; tidy.

--- src/Onsang/UI/BasePropView.cpp
+++ src/Onsang/UI/BasePropView.cpp
@@ -34,6 +34,7 @@ add_base_prop_view(
 	// Slug property
 	auto field_slug = UI::Field::make(root, object.get_slug());
 	field_slug->get_geometry().set_sizing(UI::Axis::horizontal, UI::Axis::horizontal);
+	UI::bind_field_describer(field_slug, "slug");
 	field_slug->signal_user_modified.bind([&session, &object](
 		UI::Field::SPtr field_slug,
 		bool const accept
@@ -47,7 +48,6 @@ add_base_prop_view(
 			field_slug->set_text(object.get_slug());
 		}
 	});
-	UI::bind_field_describer(field_slug, "slug");
 
 	// Metadata property
 	auto grid_metadata = UI::TableGrid::make(
@@ -55,22 +55,61 @@ add_base_prop_view(
 		object.get_metadata().table()
 	);
 	UI::bind_field_describer(grid_metadata, "metadata");
+	grid_metadata->signal_cell_edited.bind([&session, &object](
+		Hord::Data::Table::Iterator& it,
+		UI::index_type col,
+		String const& string_value,
+		Hord::Data::ValueRef& new_value
+	) {
+		if (col == 0) {
+			Hord::Cmd::Object::RenameMetaField{session}(
+				object, it.index, string_value
+			);
+		} else if (col == 1) {
+			Hord::Cmd::Object::SetMetaField{session}(
+				object, it.index, new_value
+			);
+		}
+	});
 
 	auto view = UI::PropView::make(root, "base", UI::Axis::vertical);
 	auto& field_slug_ref = *field_slug;
-	view->signal_notify_command.bind([&object, &field_slug_ref](
+	auto& grid_metadata_ref = *grid_metadata;
+	view->signal_notify_command.bind([&object, &field_slug_ref, &grid_metadata_ref](
 		UI::View* const /*parent_view*/,
 		UI::PropView& /*prop_view*/,
 		Hord::Cmd::UnitBase const& command,
 		Hord::Cmd::type_info const& type_info
 	) {
-		if (command.get_object_id() != object.get_id()) {
+		if (
+			command.get_object_id() != object.get_id() ||
+			!command.ok_action()
+		) {
 			return;
 		}
 		switch (type_info.id) {
 		case Hord::Cmd::Object::SetSlug::COMMAND_ID:
 			field_slug_ref.set_text(object.get_slug());
 			break;
+
+		case Hord::Cmd::Object::SetMetaField::COMMAND_ID: {
+			auto const& c = static_cast<Hord::Cmd::Object::SetMetaField const&>(command);
+			if (c.created()) {
+				grid_metadata_ref.insert_before(c.get_field_index(), 1);
+			} else {
+				grid_metadata_ref.notify_cell_changed(c.get_field_index(), 1);
+			}
+		}	break;
+
+		case Hord::Cmd::Object::RenameMetaField::COMMAND_ID: {
+			auto const& c = static_cast<Hord::Cmd::Object::RenameMetaField const&>(command);
+			grid_metadata_ref.notify_cell_changed(c.get_field_index(), 0);
+		}	break;
+
+		case Hord::Cmd::Object::RemoveMetaField::COMMAND_ID: {
+			auto const& c = static_cast<Hord::Cmd::Object::RemoveMetaField const&>(command);
+			grid_metadata_ref.erase(c.get_field_index(), 1);
+		}	break;
 		}
 	});
 
